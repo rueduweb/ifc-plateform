a4783673dee4b757b5a91a9aa247c626cd23b02f
rueduweb <rhu.sab@outlook.fr>
2025-06-07 20:33:07 +0200

    add 3 components and beginning routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ifc-plateform';
+  menu = [
+    'équipe', 'championnat', 'staff', 'partenaires', 'inside', 'contact'
+  ];
 }
---
+++ src/app/components/championship/championship.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-championship',
+  imports: [],
+  templateUrl: './championship.component.html',
+  styleUrl: './championship.component.scss'
+})
+export class ChampionshipComponent {
+
+}
